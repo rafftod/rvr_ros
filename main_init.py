dbaf2013c8081d2a187222f39cc98dfad7464712
jepsiko <arthur.pierrot@hotmail.com>
2022-08-10 12:29:25 +0200

    color saturation

--- main_init.py
+++ main_init.py
@@ -4,6 +4,14 @@ import sys
 
 UPDATE_INTERVAL = 0.5
 
+def saturate(colors):
+    max_color = max(colors)
+    factor = 255 / max_color
+    for i in range(len(colors)):
+        colors[i] = int(colors[i] * factor)
+        if colors[i] > 255:
+            colors[i] = 255
+
 if len(sys.argv) != 2:
     raise ValueError("Port not given")
 
@@ -17,8 +25,11 @@ while True:
         with open('floor_color') as floor_color:
             try:
                 red, green, blue = floor_color.readline().strip().split()
-                device.set_color(red, green, blue)
+                colors = [red, green, blue]
+                saturate(colors)
+                device.set_color(colors[0], colors[1], colors[2])
                 device.send_transmission()
-                last_update = now
+                
             except Exception as e:
-                print(e)
+                pass
+            last_update = now
